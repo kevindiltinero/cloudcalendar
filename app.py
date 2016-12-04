ea6692f394dfafc31bb72fae54fe833172f922b3
kevinfitzpatrick <mastery414@gmail.com>
2016-12-04 17:36:46 +0000

    All of it

--- app.py
+++ app.py
@@ -81,104 +81,99 @@ def remove_event(eventid):
 ##########################    INSTANTIATE THE FLASK APP  ##################################
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    conn = sqlite3.connect('calendar.db')
-    c = conn.cursor()
-    return "hello"
 
 
 # ##########################                MAIN PAGE               ##################################
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     database_to_json()
-#
-#     # If the form was used get the day and hour and do the above prediction with that day and hour
-#     if request.method == 'POST':
-#         #GET ALL VALUES FROM AJAX
-#         title = request.json['title']
-#         category = request.json['categor']
-#         eventstart = request.json['start']
-#         eventend = request.json['start']
-#         eventid = request.json['eventid']
-#         print(title, category, eventstart, eventend, eventid)
-#
-#         #WRITE INTO THE DATABASE
-#         event_writting(eventid, title, eventstart, eventend, category)
-#         database_to_json()
-#         # Send over the events and the predictions results over to the main page to plot colors and icons.
-#         return render_template('index.html')
-#     database_to_json()
-#     return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    database_to_json()
+
+    # If the form was used get the day and hour and do the above prediction with that day and hour
+    if request.method == 'POST':
+        #GET ALL VALUES FROM AJAX
+        title = request.json['title']
+        category = request.json['categor']
+        eventstart = request.json['start']
+        eventend = request.json['start']
+        eventid = request.json['eventid']
+        print(title, category, eventstart, eventend, eventid)
+
+        #WRITE INTO THE DATABASE
+        event_writting(eventid, title, eventstart, eventend, category)
+        database_to_json()
+        # Send over the events and the predictions results over to the main page to plot colors and icons.
+        return render_template('index.html')
+    database_to_json()
+    return render_template('index.html')
 #
 #
 #
 # ########################## EVENTS PAGE  ##################################
-# @app.route('/events', methods=['POST', 'GET'])
-# #This is simply the rendering of the page from the navigation bar. See form functionality below.
-# def events():
-#     # If the form was used get the day and hour and do the above prediction with that day and hour
-#     if request.method == 'POST':
-#         newtitle = request.json['newtitle']
-#         newcategory = request.json['newcategory']
-#         oldstart = request.json['oldstart']
-#         oldtitle = request.json['oldtitle']
-#         eventid = request.json['eventid']
-#         print(newtitle, newcategory, oldstart, oldtitle)
-#         updateevent(eventid, newtitle, newcategory)
-#
-#         # Send over the events and the predictions results over to the main page to plot colors and icons.
-#         return render_template('index.html')
-#     # database_to_json()
-#     return render_template('index.html')
-#
-#
-#
-# @app.route('/query', methods=['POST', 'GET'])
-# #This is simply the rendering of the page from the navigation bar. See form functionality below.
-# def query():
-#     # If the form was used get the day and hour and do the above prediction with that day and hour
-#     if request.method == 'POST':
-#         queryevent = request.json['queryevent']
-#         print(queryevent)
-#
-#         # Send over the events and the predictions results over to the main page to plot colors and icons.
-#         return render_template('index.html')
-#     # database_to_json()
-#     return render_template('index.html')
+@app.route('/events', methods=['POST', 'GET'])
+#This is simply the rendering of the page from the navigation bar. See form functionality below.
+def events():
+    # If the form was used get the day and hour and do the above prediction with that day and hour
+    if request.method == 'POST':
+        newtitle = request.json['newtitle']
+        newcategory = request.json['newcategory']
+        oldstart = request.json['oldstart']
+        oldtitle = request.json['oldtitle']
+        eventid = request.json['eventid']
+        print(newtitle, newcategory, oldstart, oldtitle)
+        updateevent(eventid, newtitle, newcategory)
+
+        # Send over the events and the predictions results over to the main page to plot colors and icons.
+        return render_template('index.html')
+    # database_to_json()
+    return render_template('index.html')
 #
 #
 #
-# @app.route('/drag', methods=['POST', 'GET'])
-# def drag():
-#     # If the form was used get the day and hour and do the above prediction with that day and hour
-#     if request.method == 'POST':
-#         eventid = request.json['eventid']
-#         eventstart = request.json['eventstart']
-#         print(eventid, eventstart)
-#         updatedate(eventid, eventstart)
-#
-#         # Send over the events and the predictions results over to the main page to plot colors and icons.
-#         return render_template('index.html')
-#     # database_to_json()
-#     return render_template('index.html')
-#
+@app.route('/query', methods=['POST', 'GET'])
+#This is simply the rendering of the page from the navigation bar. See form functionality below.
+def query():
+    # If the form was used get the day and hour and do the above prediction with that day and hour
+    if request.method == 'POST':
+        queryevent = request.json['queryevent']
+        print(queryevent)
+
+        # Send over the events and the predictions results over to the main page to plot colors and icons.
+        return render_template('index.html')
+    # database_to_json()
+    return render_template('index.html')
 #
 #
 #
-# @app.route('/remove', methods=['POST', 'GET'])
-# def remove():
-#     # If the form was used get the day and hour and do the above prediction with that day and hour
-#     if request.method == 'POST':
-#         eventid = request.json['eventid']
-#         print(eventid)
-#         remove_event(eventid)
-#         # updatedate(eventid, eventstart)
+@app.route('/drag', methods=['POST', 'GET'])
+def drag():
+    # If the form was used get the day and hour and do the above prediction with that day and hour
+    if request.method == 'POST':
+        eventid = request.json['eventid']
+        eventstart = request.json['eventstart']
+        print(eventid, eventstart)
+        updatedate(eventid, eventstart)
+
+        # Send over the events and the predictions results over to the main page to plot colors and icons.
+        return render_template('index.html')
+    # database_to_json()
+    return render_template('index.html')
 #
-#         # Send over the events and the predictions results over to the main page to plot colors and icons.
-#         return render_template('index.html')
-#     # database_to_json()
-#     return render_template('index.html')
+#
+#
+#
+@app.route('/remove', methods=['POST', 'GET'])
+def remove():
+    # If the form was used get the day and hour and do the above prediction with that day and hour
+    if request.method == 'POST':
+        eventid = request.json['eventid']
+        print(eventid)
+        remove_event(eventid)
+        # updatedate(eventid, eventstart)
+
+        # Send over the events and the predictions results over to the main page to plot colors and icons.
+        return render_template('index.html')
+    # database_to_json()
+    return render_template('index.html')
 #
 
 
